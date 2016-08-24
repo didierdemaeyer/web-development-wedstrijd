aee7b4d6ae15ae2171cc4e0d8d431cd6f1b662ce
Didier De Maeyer <didierdemaeyer@gmail.com>
2016-08-25 01:18:35 +0200

    ClientInfoHelper fix

--- app/Helpers/ClientInfoHelper.php
+++ app/Helpers/ClientInfoHelper.php
@@ -6,7 +6,7 @@
 function getClientIpAddress() {
     $ipAddress = $_SERVER['REMOTE_ADDR'];
     if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
-        $ipAddress = array_pop(explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']));
+        $ipAddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
     }
 
     return $ipAddress;
