9d955d9f273bebd6a5ee7987c96fe505f2af9951
Didier De Maeyer <didierdemaeyer@gmail.com>
2016-08-27 01:03:23 +0200

    updated contest periods

--- database/seeds/ContestPeriodsTableSeeder.php
+++ database/seeds/ContestPeriodsTableSeeder.php
@@ -16,33 +16,23 @@ class ContestPeriodsTableSeeder extends Seeder
         $periods = [
             [
                 'period_number' => 1,
-                'startdate' => '2016-08-23 00:00:00',
-                'enddate' => '2016-08-23 23:59:59',
+                'startdate' => '2016-08-27 00:00:00',
+                'enddate' => '2016-08-28 23:59:59',
             ],
             [
                 'period_number' => 2,
-                'startdate' => '2016-08-24 00:00:00',
-                'enddate' => '2016-08-24 23:59:59',
+                'startdate' => '2016-08-29 00:00:00',
+                'enddate' => '2016-08-30 23:59:59',
             ],
             [
                 'period_number' => 3,
-                'startdate' => '2016-08-25 00:00:00',
-                'enddate' => '2016-08-25 23:59:59',
+                'startdate' => '2016-08-31 00:00:00',
+                'enddate' => '2016-09-01 23:59:59',
             ],
             [
                 'period_number' => 4,
-                'startdate' => '2016-08-26 00:00:00',
-                'enddate' => '2016-08-26 23:59:59',
-            ],
-            [
-                'period_number' => 5,
-                'startdate' => '2016-08-27 00:00:00',
-                'enddate' => '2016-08-27 23:59:59',
-            ],
-            [
-                'period_number' => 6,
-                'startdate' => '2016-08-28 00:00:00',
-                'enddate' => '2016-08-28 23:59:59',
+                'startdate' => '2016-09-02 00:00:00',
+                'enddate' => '2016-09-03 23:59:59',
             ],
         ];
 
