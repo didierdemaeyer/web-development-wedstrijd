05d5b1d0224c0c84fbd595baade939bef69f111b
Didier De Maeyer <didierdemaeyer@gmail.com>
2016-08-26 03:07:29 +0200

    thank-you page design

--- resources/views/participate/thank-you.blade.php
+++ resources/views/participate/thank-you.blade.php
@@ -3,11 +3,13 @@
 @section('title', 'Thank you for participating')
 
 @section('content')
-  <div id="participate-container" class="container">
+  <div id="participate-container">
 
+    <div class="form-container">
       <h1 class="big">Thank you for participating</h1>
 
       <p>View your photo: <a href="{{ route('entries.show', session('photo_id')) }}">Link</a></p>
 
+    </div>
   </div>
 @stop
